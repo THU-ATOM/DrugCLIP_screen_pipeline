37f8c3e87feaccccab1de2f0cb3eafde28e66b07
jyj07 <jyj20@mails.tsinghua.edu.cn>
2024-03-19 17:38:00 +0800

    pocket and mol debugged

---
+++ mol_from_tsv.py
@@ -0,0 +1,133 @@
+import pandas as pd
+import argparse
+import multiprocessing as mp
+import os
+import pickle
+from random import shuffle
+import lmdb
+import numpy as np
+import pandas as pd
+import rdkit
+from tqdm import tqdm
+from rdkit import Chem, RDLogger
+from rdkit.Chem import AllChem
+from rdkit.Chem.MolStandardize import rdMolStandardize
+
+ACTIVE_CODE = ['active','weak activity','slightly active','slight inhibition','potent inhibitor','partially active','partial antagonist','partial agonist','non-competitive antagonist','inverse agonist','irreversible antagonist','dose-dependent effect','antagonist','agonist','activator']
+INACTIVE_CODE = ['no significant effect','no significant activity','no effect','no activity','no action','inhibition not detected','inactive']
+def tsv2smiles(tsvfile,thres_active=10000,thres_inactive=20000):
+
+    dataframe = pd.read_csv(tsvfile, sep="\t",quotechar = '"')
+    ids = dataframe['Molecule ChEMBL ID'].tolist()
+    smi = dataframe['Smiles'].tolist()
+    relation = dataframe['Standard Relation'].tolist()
+    value = dataframe['Standard Value'].tolist()
+    unites = dataframe['Standard Units'].tolist()
+    comments = dataframe['Comment'].tolist()
+    active = set({})
+    inactive = set({})
+    not_determined= set({})
+    for n,s,r,v,u,c in zip(ids,smi,relation,value,unites,comments):
+        if str(c).lower() in ACTIVE_CODE:#respect expert opinions
+            active.add((n,s))
+        elif str(c).lower() in INACTIVE_CODE or 'not active' in str(c).lower() or 'no inhibit' in str(c).lower():
+            inactive.add((n,s))
+        else:        
+            if u != 'nM':
+                not_determined.add((n,s))
+            else:
+                if r != "'>'" and v <= thres_active:
+                    active.add((n,s))
+                elif r != "'<'" and v >= thres_inactive:
+                    inactive.add((n,s))
+                else:
+                    not_determined.add((n,s))
+    return active,inactive,not_determined
+
+def write_lmdb(data, lmdb_path, num):
+    #resume
+    
+    env = lmdb.open(lmdb_path, subdir=False, readonly=False, lock=False, readahead=False, meminit=False, map_size=1099511627776)
+    with env.begin(write=True) as txn:
+        for d in data:
+            txn.put(str(num).encode('ascii'), pickle.dumps(d))
+            num += 1
+def gen_conformation(smiles, num_conf=1, num_worker=1):
+    if len(smiles)>100:
+        print("exceede max smiles lens", smiles)
+        return None
+    mol = Chem.MolFromSmiles(smiles)
+    try:
+        mol = Chem.AddHs(mol)
+        AllChem.EmbedMultipleConfs(mol, numConfs=num_conf, numThreads=num_worker, pruneRmsThresh=1, maxAttempts=1000, useRandomCoords=False)
+        AllChem.MMFFOptimizeMoleculeConfs(mol, numThreads=num_worker)
+        mol = Chem.RemoveHs(mol)
+    except:
+        print("cannot gen conf", smiles)
+        return None
+    if mol.GetNumConformers() == 0:
+        print("cannot gen conf", smiles)
+        return None
+    return mol
+
+def convert_2Dmol_to_data(name,simles, num_conf=1, num_worker=1):
+    #to 3D
+    mol = gen_conformation(simles, num_conf, num_worker)
+    if mol is None:
+        return None
+    coords = [np.array(mol.GetConformer(i).GetPositions()) for i in range(mol.GetNumConformers())]
+    atom_types = [a.GetSymbol() for a in mol.GetAtoms()]
+    return {'coordinates': coords, 'atoms': atom_types, 'smi': name+'_'+simles, 'mol': mol}
+
+def multiprocess_confgen(smi_list,n_cpu=32):
+    result = []
+    tbar = tqdm(total=len(smi_list))
+    def call_back(r):
+        tbar.update(1)
+        if r is not None:
+            result.append(r)
+    pool = mp.Pool(n_cpu)
+    shuffle(smi_list)
+    for n,s in smi_list:
+        pool.apply_async(func=convert_2Dmol_to_data,args=(n,s,),callback=call_back)
+    pool.close()
+    pool.join()   
+    return result 
+
+def process_one_chembldir(dirs):
+    active = set({})
+    inactive = set({})
+    not_determined= set({})
+    for d in os.listdir(dirs):
+        if '.tsv' in d:
+            a,i,u = tsv2smiles(os.path.join(dirs,d))
+            active.update(a)
+            inactive.update(i)
+            not_determined.update(u)
+    active_x_inactive = active.intersection(inactive)
+    active.difference_update(active_x_inactive)
+    inactive.difference_update(active_x_inactive)
+    not_determined.update(active_x_inactive)    
+    active_mol = multiprocess_confgen(list(active))
+    inactive_mol=multiprocess_confgen(list(inactive))
+    not_determined_mol=multiprocess_confgen(list(not_determined))
+    write_lmdb(active_mol, os.path.join(dirs,'active.lmdb'), 0)
+    write_lmdb(inactive_mol, os.path.join(dirs,'inactive.lmdb'), 0)
+    write_lmdb(not_determined_mol, os.path.join(dirs,'not_determined.lmdb'), 0)
+    return len(active_mol),len(inactive_mol),len(not_determined_mol),len(active_x_inactive)
+
+def PubChem_csv(csv_file):
+    mols = set({})
+    for lines in open(csv_file):
+        cid = lines.split(',')[2]
+        smi = lines.split(',')[3]
+        if len(smi) != 0:
+            mols.add((cid,smi))
+    mol_data = multiprocess_confgen(list(mols))
+    write_lmdb(mol_data, csv_file.replace('.csv','.lmdb'), 0)
+
+if __name__ == '__main__':
+    #PubChem_csv('/home/jiayinjun/Chem_Aware_Interaction_Understanding/data/SARS_PLP/PubChem/SARS_PLP_inactive.csv')
+    process_one_chembldir('/newhome/jiayj/DrugCLIP_screen_pipeline/targets/GammaSecretase/ChEMBL')
+
+
---
+++ pocket_from_pdb.py
@@ -0,0 +1,109 @@
+import os
+from Bio.PDB import PDBParser,Chain,Model,Structure
+from Bio.PDB.PDBIO import PDBIO
+from Bio.PDB import is_aa
+from Bio.PDB.Residue import DisorderedResidue,Residue
+from Bio.PDB.Atom import DisorderedAtom
+import warnings
+from Bio.PDB.StructureBuilder import PDBConstructionWarning
+from tqdm import tqdm
+import numpy as np
+import lmdb
+import numpy as np
+import pickle
+import re
+
+def write_lmdb(data, lmdb_path, num):
+    #resume
+    
+    env = lmdb.open(lmdb_path, subdir=False, readonly=False, lock=False, readahead=False, meminit=False, map_size=1099511627776)
+    with env.begin(write=True) as txn:
+        for d in data:
+            txn.put(str(num).encode('ascii'), pickle.dumps(d))
+            num += 1
+    
+    return num
+
+warnings.filterwarnings(
+    action='ignore',
+    category=PDBConstructionWarning)
+def extract_lig_recpt(biopy_model,ligname):
+    lig_list = []
+    tmp_chain = Chain.Chain('A') 
+    rid = 0
+    for chain in biopy_model:
+        for res in chain:
+            res.detach_parent()
+            if not(is_aa(res,standard=True)):
+                if res.resname == ligname:
+                    lig_list.append((chain.id+'_'+str(res.id[1]),res))
+                continue
+            if res.is_disordered():
+                if isinstance(res,DisorderedResidue):
+                    res = res.selected_child
+                    res.id = (res.id[0],rid,res.id[2])
+                    rid += 1 
+                    tmp_chain.add(res.copy())      
+                else:
+                    new_res = Residue(res.id,res.resname,res.segid)
+                    for atom in res:
+                        if isinstance(atom,DisorderedAtom):  
+                            atom.selected_child.disordered_flag = 0
+                            new_res.add(atom.selected_child.copy())
+                        else:
+                            new_res.add(atom)
+                    res = new_res
+                    res.id = (res.id[0],rid,res.id[2])
+                    rid += 1 
+                    tmp_chain.add(res.copy())       
+            else:
+                res.id = (res.id[0],rid,res.id[2])
+                rid += 1 
+                tmp_chain.add(res.copy())
+    # tmp_structure = Structure.Structure('tmp')
+    # tmp_model = Model.Model(0)
+    # tmp_structure.add(tmp_model) 
+    # tmp_model.add(tmp_chain)
+  
+    return tmp_chain,lig_list
+
+def get_binding_pockets(biopy_chain,liglist):
+    pockets = []
+    for n,lig in liglist:
+            lig_coord = np.array([i.coord for i in lig.get_atoms()])
+            tmp_chain = Chain.Chain('A') 
+            for res in biopy_chain:
+                res_coord = np.array([i.get_coord() for i in res.get_atoms()])
+                dist = np.linalg.norm(res_coord[:,None,:]-lig_coord[None,:,:],axis=-1).min()
+                if dist<=6:
+                    tmp_chain.add(res.copy())
+            pockets.append((n,tmp_chain))
+    return pockets  
+
+def pocket2lmdb(pocket_name,biopy_chain,pdb_name):
+    recpt = list(biopy_chain.get_atoms())
+    pocket_atom_type = [x.element for x in recpt]
+    pocket_coord = [x.coord for x in recpt]
+    return {
+        'pocket': pdb_name+'_'+pocket_name,
+        'pocket_atoms': pocket_atom_type,
+        'pocket_coordinates': pocket_coord
+    }
+
+
+def process_one_pdbdir(dirs):
+    all_pocket = []
+    for d in os.listdir(dirs):
+        if '.pdb' in d:
+            try:
+                p = PDBParser()
+                model = p.get_structure('0',os.path.join(dirs,d))[0]  
+                tmp_chain,liglist = extract_lig_recpt(model,re.search(r'_.*\.',d)[0][1:-1])
+                pocket = get_binding_pockets(tmp_chain,liglist)
+                pocket = [pocket2lmdb(n,p,d.split('.')[0]) for n,p in pocket]
+                all_pocket += pocket
+            except:
+                pass
+    write_lmdb(all_pocket,os.path.join(dirs,'pocket.lmdb'),0)
+if __name__ == '__main__':
+    process_one_pdbdir('/newhome/jiayj/DrugCLIP_screen_pipeline/targets/GammaSecretase/Pocket1')
